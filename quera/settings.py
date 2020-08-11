34fb2ed0ad88ed531774124bb1a4cc45c4d6612f
_H_E_R_T_Z_ <hashemzargari@gmail.com>
2020-08-11 12:49:16 +0430

    install django-webpack-loader + downgrade webpack_bundle_tracker to 0.4.3 because of conflict to d-w-l

--- quera/settings.py
+++ quera/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'rest_auth.registration',
 
     'crispy_forms',
+    'webpack_loader',
 
     'users',
     'questions',
@@ -161,3 +162,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 3
 }
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'dist/',
+        'STATS_FILE': BASE_DIR / 'frontend/webpack-stats.json',
+    }
+}
+
+print(WEBPACK_LOADER['DEFAULT']['STATS_FILE'])
